f927675f67e91aaa299250052eed260ad9efce25
baysidev <100793313+baysidev@users.noreply.github.com>
2022-03-16 15:10:54 +0300

    Update asaycheck.java

--- asaycheck.java
+++ asaycheck.java
@@ -33,3 +33,40 @@ public class asaycheck {
         isAsal();
     }
 }
+
+/* 
+package Giris.metot;
+
+import java.util.Scanner;
+
+public class asaycheck {
+
+   static void isAsal(int sayi){
+
+       int count = 0;
+
+       for (int i = 1; i < sayi; i++){
+           for (int j = 1; j < i; j++){
+               if(i % j == 0){
+                   count++;
+               }
+           }
+       }
+
+       if (count < 2){
+           System.out.println(sayi+" asal sayıdır.");
+       } else {
+           System.out.println(sayi+" asal sayı değildir.");
+       }
+
+   }
+
+    public static void main(String[] args) {
+
+        isAsal(27);
+        isAsal(2);
+        isAsal(17);
+    }
+}
+
+*/
