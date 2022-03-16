0fa95bf2a6ae14e9548dcf7b3e33c6f43a6e74d5
baysidev <100793313+baysidev@users.noreply.github.com>
2022-03-16 15:07:04 +0300

    Add files via upload

---
+++ asaycheck.java
@@ -0,0 +1,35 @@
+package Giris.metot;
+
+import java.util.Scanner;
+
+public class asaycheck {
+
+   static void isAsal(){
+
+       int sayi;
+       int count = 0;
+       Scanner giris = new Scanner(System.in);
+       System.out.print("Sayı giriniz: ");
+       sayi = giris.nextInt();
+
+       for (int i = 1; i < sayi; i++){
+           for (int j = 1; j < i; j++){
+               if(i % j == 0){
+                   count++;
+               }
+           }
+       }
+
+       if (count < 2){
+           System.out.println(sayi+" asal sayıdır.");
+       } else {
+           System.out.println(sayi+" asal sayı değildir.");
+       }
+
+   }
+
+    public static void main(String[] args) {
+
+        isAsal();
+    }
+}
